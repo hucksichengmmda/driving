c7a095fb34db0f9323f46d0a38cfa7a7ad794f6d
rensicheng <472979947@qq.com>
2018-12-26 00:32:08 +0800

    Add US part

---
+++ test5.py
@@ -0,0 +1,13 @@
+country = input('请问你的国家:')
+age = input('你的年龄:')
+age = int(age)
+if country == '台湾':
+	if age >= 18:
+		print('你可以考驾照！')
+	else:
+		print('你还不可以考驾照哦！')
+elif country == '美国':
+	if age >= 16:
+		print('你可以考驾照了！')
+	else:
+		print('你还不可以考驾照哦')
